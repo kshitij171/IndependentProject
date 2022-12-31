b3a9fa7e447b2de83e22312643240adb3becdc93
kshitij171 <88544175+kshitij171@users.noreply.github.com>
2022-12-31 08:17:56 +0530

    cards introduced

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+// import './App.css';
 import Head from './components/Head';
 import Body from './components/Body';
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ export default function Head() {
   return (
     <div className='grid-container'>
         <div className='photo'></div>
-        <div className='paragraph'>REN Profile & Facts LØREN (로렌) is a solo singer, DJ and producer under YG Entertainment‘s sub-label, THEBLACKLABEL. He made his debut on Novemb</div>
+        <div className='paragraph'>REN Profile & Facts LØREN (로렌) is ax solo singer, DJ and producer under YG Entertainment‘s sub-label, THEBLACKLABEL. He made his debut on Novemb</div>
     </div>
   )
 }
