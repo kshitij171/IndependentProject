b50c01077aa435d9fb4f1ff2d6dbc8b1ea98796a
amit20493 <88581489+amit20493@users.noreply.github.com>
2023-02-18 03:04:10 +0530

    full responsive contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,30 +1,42 @@
-import { Box,TextInput, Textarea, Group, Title, Button } from '@mantine/core';
+import { Box,TextInput, Textarea, Group, Title, Button,Alert} from '@mantine/core';
 import { useForm } from '@mantine/form';
-import React from "react";
+import React, { useState } from "react";
+
+import emailjs from 'emailjs-com';
 
 export function Contact() {
-    const form = useForm({
-      initialValues: {
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      },
-      validate: {
-        name: (value) => value.trim().length < 2,
-        email: (value) => !/^\S+@\S+$/.test(value),
-        subject: (value) => value.trim().length === 0,
-        message: (value) => value.trim().length === 0,
-      },
-    });
-    // this is form spree link will use to connect to a test email
-  //"https://formspree.io/f/xlekjdba"
-    return (
-        <Box mt={'80px'} pt={'sm'} ml={'100px'} mr={'100px'}>
-            <form 
-                action="https://formspree.io/f/xlekjdba"
-                method="POST">
-                <Title
+  // const theme = useMantineTheme();
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [subject,setSubject] =useState("");
+
+
+  
+  const [alert, setAlert] = useState(null);
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      emailjs.sendForm('service_vp3s1gk', 'template_4162x84', e.target, 'rWfAwNYPsYMhYqP5V')
+      .then((result) => {
+          console.log(result.text);
+          setName('');
+          setEmail('');
+          setMessage('');
+          setSubject('');
+          setAlert({ type: 'success', message: 'Your message has been sent!' });
+      }, (error) => {
+          console.log(error.text);
+          setAlert({ type: 'error', message: 'Oops, something went wrong. Please try again!' });
+      });
+  };
+  
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box mt={'80px'} pt={'sm'} ml={'100px'} mr={'100px'}>
+      <Title
                     order={1}
                     size="h1"
                     sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
@@ -32,57 +44,59 @@ export function Contact() {
                     align="center"
                 >
                 Contact us
-               </Title>
-  
-                
-               <TextInput
-                        label="Name"
-                        placeholder="Your name"
-                        name="name"
-                        variant="filled"
-                        withAsterisk
-                        {...form.getInputProps('name')}
-                />
+      </Title>
+      <TextInput
+        type="text"
+        label="Name"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        
+      />
+      <TextInput
+        
+        label="Email"
+        name="email"
+        type="email"
+        value ={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
 
-                <TextInput
-                        label="Email"
-                        placeholder="Your Email ID"
-                        name="name"
-                        variant="filled"
-                        withAsterisk
-                        {...form.getInputProps('email')}
-                />
-                
-  
-                <TextInput
-                    label="Subject"
-                    placeholder="Subject"
-                    mt="md"
-                    name="subject"
-                    variant="filled"
-                    withAsterisk
-                    {...form.getInputProps('subject')}
-                />
-                <Textarea
-                    mt="md"
-                    label="Message"
-                    placeholder="Your message"
-                    maxRows={20}
-                    minRows={5}
-                    autosize
-                    name="message"
-                    variant="filled"
-                    withAsterisk
-                    {...form.getInputProps('message')}
-                    
-                />
-  
-                <Group position="center" mt="xl">
-                <Button color="teal" type="submit" radius="md">
+      />
+      <TextInput
+        
+        label="Subject"
+        name="subject"
+        required
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+       
+        mt="md"
+      />
+        
+      <Textarea
+       
+        label="Message"
+        name="message"
+        required
+        maxRows={20}
+        minRows={5}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <Group position="center" mt="xl">
+          <Button color="teal" type="submit" radius="md">
                     Submit
-                </Button>
-                </Group>
-            </form>
-        </Box>
-    );
-  }
+          </Button>
+          {alert && <Alert color={alert.type} onClose={() => setAlert(null)}>{alert.message}</Alert>}
+      
+      </Group>
+      
+       
+     
+      </Box>
+    </form>
+  );
+};
+
